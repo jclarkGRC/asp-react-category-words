3f09ce3b7eb1dc756ce91d2bd0af643d30d154a7
Joshua Clark <joshua.clark@tee2.org>
2019-08-08 17:43:41 -0700

    populating select list with categories from db

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
-import { actionCreators } from '../store/Home';
+import {actionCreators} from '../store/Home';
 
 class Home extends React.Component {
+
     componentWillMount() {
         // This method runs when the component is first added to the page
         this.props.requestCategories();
     }
 
     render() {
-        console.log(this.props);
+        let {categories} = this.props;
+        const category = categories.map((category) => {
+                return (
+                    <option key={category.id}>{category.categoryName}</option>
+                )
+            }
+        );
         return (
             <div>
                 <h1>Welcome to Category Words!</h1>
@@ -18,7 +25,9 @@ class Home extends React.Component {
                     topic chosen at the beginning of the game. The trick is you only have 30 seconds for each guess. If
                     you can't guess any more words your time is up! Good Luck!</p>
                 <p>Please choose a category to start playing...</p>
-
+                <select>
+                    {category}
+                </select>
             </div>
         )
     }
